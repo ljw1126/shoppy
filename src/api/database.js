c5b9ca6ecd4e74530abb32d6eb487691cad148d4
leejinwoo <leejinwoo1126@gmail.com>
2024-06-08 18:01:47 +0900

    feat: carts/ CRUD 추가

--- src/api/database.js
+++ src/api/database.js
@@ -1,4 +1,4 @@
-import {get, getDatabase, ref, set} from "firebase/database";
+import {get, getDatabase, ref, remove, set, update} from "firebase/database";
 import {app} from "./firebase_config";
 import {v4 as uuid} from "uuid";
 
@@ -39,4 +39,48 @@ export async function getProducts() {
 
         return [];
     });
+}
+
+export async function getCarts(uid) {
+    return await get(ref(database, `carts/${uid}`))
+        .then((snapshot) => {
+            if (snapshot.exists()) {
+                return snapshot.val();
+            }
+
+            return {};
+        })
+}
+
+/**
+ * 1. 동일한 "상품_옵션" 있는 경우 update
+ * 2. 없는 경우 카트에 상품 추가
+ */
+export async function addToCart(userId, product) {
+    const cartRef = ref(database, `carts/${userId}/${getCartItemId(product)}`);
+
+    await get(cartRef).then(snapshot => {
+        if (snapshot.exists()) {
+            const items = snapshot.val();
+            const updatedQuantity = product.quantity + items.quantity;
+            const updateTotalPrice = updatedQuantity * items.price;
+
+            update(cartRef, {...items, quantity: updatedQuantity, totalPrice: updateTotalPrice});
+            return;
+        }
+
+        set(cartRef, product);
+    });
+}
+
+export async function updateToCart(userId, product) {
+    await update(ref(database, `carts/${userId}/${getCartItemId(product)}`), product);
+}
+
+export async function removeFromCart(userId, product) {
+    await remove(ref(database, `carts/${userId}/${getCartItemId(product)}`));
+}
+
+function getCartItemId(product) {
+    return `${product.id}_${product.option}`;
 }
